f17ea3335fe4bf7eb17ee9d9cdc1eca5bf637bbb
mrkirby153 <mr.austinwhyte@gmail.com>
2016-05-17 19:28:34 -0700

    Tweaks to joins/parts

--- src/main/java/me/mrkirby153/kcuhc/handler/GameListener.java
+++ src/main/java/me/mrkirby153/kcuhc/handler/GameListener.java
@@ -2,7 +2,6 @@ package me.mrkirby153.kcuhc.handler;
 
 import me.mrkirby153.kcuhc.UHC;
 import me.mrkirby153.kcuhc.arena.TeamHandler;
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -14,6 +13,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.potion.PotionEffectType;
 
 public class GameListener implements Listener {
 
@@ -27,6 +27,7 @@ public class GameListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         UHC.arena.playerJoin(event.getPlayer());
+        event.setJoinMessage("");
         if (TeamHandler.getTeamForPlayer(event.getPlayer()) == null)
             TeamHandler.joinTeam(TeamHandler.spectatorsTeam(), event.getPlayer());
         else
@@ -35,7 +36,7 @@ public class GameListener implements Listener {
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
-        event.setQuitMessage(ChatColor.RED + event.getPlayer().getName() + " has left!");
+        event.setQuitMessage("");
         UHC.arena.playerDisconnect(event.getPlayer());
     }
 
@@ -71,8 +72,11 @@ public class GameListener implements Listener {
 
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event){
+        UHC.arena.spectate(event.getPlayer());
         Bukkit.getServer().getScheduler().runTaskLater(UHC.plugin, ()->{
-            UHC.arena.spectate(event.getPlayer());
+            if(!event.getPlayer().hasPotionEffect(PotionEffectType.INVISIBILITY)){
+                UHC.arena.spectate(event.getPlayer());
+            }
         }, 10L);
     }
 }
