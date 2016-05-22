7c72bbd1c035ea23b2015c2dfa61dc5ce5f71fbe
mrkirby153 <mr.austinwhyte@gmail.com>
2016-05-22 14:48:52 -0700

    Attempt to prevent the player from spawning outside the world border

--- src/main/java/me/mrkirby153/kcuhc/handler/GameListener.java
+++ src/main/java/me/mrkirby153/kcuhc/handler/GameListener.java
@@ -2,17 +2,18 @@ package me.mrkirby153.kcuhc.handler;
 
 import me.mrkirby153.kcuhc.UHC;
 import me.mrkirby153.kcuhc.arena.TeamHandler;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.potion.PotionEffectType;
 
 public class GameListener implements Listener {
@@ -78,5 +79,51 @@ public class GameListener implements Listener {
                 UHC.arena.spectate(event.getPlayer());
             }
         }, 10L);
+    }
+
+    @EventHandler
+    public void changeWorld(PlayerChangedWorldEvent event){
+        if(!event.getFrom().getName().contains("nether")){
+            return;
+        }
+        double bounds = UHC.arena.getWorld().getWorldBorder().getSize() / 2;
+        System.out.println("Bounds: +/- "+bounds);
+        Player player = event.getPlayer();
+        if(Math.abs(player.getLocation().getBlockZ()) > bounds || Math.abs(player.getLocation().getBlockX()) > bounds){
+            System.out.println("Player has spawned outside the worldborder! Fixing");
+            // Move the player diagonally into the worldborder
+            Location toTeleport = player.getLocation().clone();
+            System.out.println(String.format("Old Location: %s - %.2f, %.2f, %.2f", toTeleport.getWorld().getName(), toTeleport.getX(), toTeleport.getY(), toTeleport.getZ()));
+            int attempts = 0;
+            while(Math.abs(toTeleport.getBlockX()) > bounds-2) {
+                if(toTeleport.getX() < 0){
+                    toTeleport.setX(toTeleport.getX() + 0.5);
+                } else {
+                    toTeleport.setX(toTeleport.getX() - 0.5);
+                }
+                attempts++;
+                if(attempts > 15000){
+                    attempts = 0;
+                    System.out.println("GIVING UP ON X");
+                    break; // Give up
+                }
+            }
+            while(Math.abs(toTeleport.getBlockZ()) > bounds-2){
+                if(toTeleport.getZ() < 0){
+                    toTeleport.setZ(toTeleport.getZ() + 0.5);
+                } else {
+                    toTeleport.setZ(toTeleport.getZ() - 0.5);
+                }
+                attempts++;
+                if(attempts > 15000){
+                    System.out.println("GIVING UP ON Z");
+                    break; // Give up
+                }
+            }
+            toTeleport = event.getPlayer().getWorld().getHighestBlockAt(toTeleport).getLocation().add(0.5, 0.5, 0.5);
+            System.out.println(String.format("New Location: %s - %.2f, %.2f, %.2f", toTeleport.getWorld().getName(), toTeleport.getX(), toTeleport.getY(), toTeleport.getZ()));
+            event.getPlayer().teleport(toTeleport);
+            event.getPlayer().sendMessage(ChatColor.GOLD+"You would've spawned outside of the world border, so we moved you back in");
+        }
     }
 }
