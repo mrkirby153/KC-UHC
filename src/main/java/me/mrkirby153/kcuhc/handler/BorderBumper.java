d874f027b314fad7dd46b833b41ed173830db3fb
mrkirby153 <mr.austinwhyte@gmail.com>
2016-06-02 14:32:28 -0700

    Better border bumping system

--- src/main/java/me/mrkirby153/kcuhc/handler/BorderBumper.java
+++ src/main/java/me/mrkirby153/kcuhc/handler/BorderBumper.java
@@ -1,10 +1,8 @@
 package me.mrkirby153.kcuhc.handler;
 
-import me.mrkirby153.kcuhc.UHC;
-import me.mrkirby153.kcuhc.UtilChat;
-import me.mrkirby153.kcuhc.arena.UHCArena;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
@@ -16,34 +14,79 @@ public class BorderBumper implements Runnable {
 
     private HashMap<UUID, Long> bumperCooldown = new HashMap<>();
 
+    private static final double BUMP_POWER = 0.25;
+
     @Override
     public void run() {
-        if(UHC.arena.currentState() != UHCArena.State.RUNNING)
-            return;
         for (Player player : Bukkit.getOnlinePlayers()) {
             if (bumperCooldown.get(player.getUniqueId()) != null) {
                 if (System.currentTimeMillis() < bumperCooldown.get(player.getUniqueId()))
                     return;
             }
-            double borderBounds = player.getWorld().getWorldBorder().getSize() / 2;
+            double borderBounds = (player.getWorld().getWorldBorder().getSize() / 2) - 0.25;
+            if(borderBounds < 10)
+                return;
             if (Math.abs(player.getLocation().getX()) > borderBounds || Math.abs(player.getLocation().getZ()) > borderBounds) {
-                // Bump them towards spawn?
-                Vector playerVector = player.getLocation().toVector();
-                Vector wbCenter = player.getWorld().getWorldBorder().getCenter().toVector();
-
-                double distanceTo = wbCenter.clone().setY(0).subtract(playerVector.clone().setY(0)).length();
-                if(distanceTo < 10)
-                    return;
-
-                Vector vector = wbCenter.subtract(playerVector).setY(0).normalize();
-                vector.setY(0.25);
-                vector.multiply(0.75);
-                player.setVelocity(vector);
-                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BASS, 1F, 1F);
-                player.spigot().sendMessage(UtilChat.generateFormattedChat("STAY INSIDE OF THE WORLD BORDER!", ChatColor.WHITE, 8));
+                bumpPlayer(player, player.getWorld().getWorldBorder().getCenter(), player.getWorld().getWorldBorder().getSize());
                 bumperCooldown.put(player.getUniqueId(), System.currentTimeMillis() + 500);
             }
         }
+    }
+
+
+    public static void bumpPlayer(Player player, Location wbCenter, double wbSize) {
+        double worldBorderX = Math.abs(wbCenter.getX() + (wbSize / 2)) - 1;
+        double worldBorderZ = Math.abs(wbCenter.getZ() + (wbSize / 2)) - 1;
+
+        double distPosX, distNegX, distPosZ, distNegZ;
+        Location playerLocation = player.getLocation();
+        distPosX = Math.abs(worldBorderX) - playerLocation.getX();
+        distNegX = Math.abs(-Math.abs(worldBorderX) - playerLocation.getX());
+
+        distPosZ = Math.abs(worldBorderZ) - playerLocation.getZ();
+        distNegZ = Math.abs(-Math.abs(worldBorderZ) - playerLocation.getZ());
+        Direction closest;
+        double closestPos = Math.min(distPosX, Math.min(distNegX, Math.min(distPosZ, distNegZ)));
+
+        if (closestPos == distPosX)
+            closest = Direction.POSITIVE_X;
+        else if (closestPos == distNegX)
+            closest = Direction.NEGATIVE_X;
+        else if (closestPos == distPosZ)
+            closest = Direction.POSITIVE_Z;
+        else if (closestPos == distNegZ)
+            closest = Direction.NEGATIVE_Z;
+        else
+            closest = Direction.UNKNOWN;
+        Vector bumpVector = new Vector(0, 0, 0);
+        switch (closest) {
+            case POSITIVE_X:
+                bumpVector.setX(-1);
+                break;
+            case NEGATIVE_X:
+                bumpVector.setX(1);
+                break;
+            case POSITIVE_Z:
+                bumpVector.setZ(-1);
+                break;
+            case NEGATIVE_Z:
+                bumpVector.setZ(1);
+                break;
+        }
+        bumpVector.multiply(0.75);
+        bumpVector.setY(0.1879);
+        player.setVelocity(bumpVector);
+        player.sendMessage(ChatColor.BOLD + "Stay inside the world border!".toUpperCase());
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BASS, 1F, 1F);
+    }
+
+
+    enum Direction {
+        UNKNOWN,
+        POSITIVE_Z,
+        NEGATIVE_Z,
+        POSITIVE_X,
+        NEGATIVE_X;
     }
 
 }
