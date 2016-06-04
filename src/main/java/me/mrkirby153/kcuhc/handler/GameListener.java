1b3518ec3f6530d5d99200b77a177b33d5797bf3
mrkirby153 <mr.austinwhyte@gmail.com>
2016-06-04 11:48:57 -0700

    Teleport the player away if they will  block interactions

--- src/main/java/me/mrkirby153/kcuhc/handler/GameListener.java
+++ src/main/java/me/mrkirby153/kcuhc/handler/GameListener.java
@@ -5,11 +5,15 @@ import me.mrkirby153.kcuhc.arena.TeamHandler;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -53,7 +57,7 @@ public class GameListener implements Listener {
         }
         double oldDamage = event.getDamage();
         double newDamage = Math.floor(oldDamage / 2);
-        if(newDamage < 1)
+        if (newDamage < 1)
             newDamage = 1;
         event.setDamage(newDamage);
         System.out.println(String.format("[DMG] OLD: [%.2f] NEW: [%.2f]", oldDamage, newDamage));
@@ -66,57 +70,86 @@ public class GameListener implements Listener {
         }
         double oldDamage = event.getDamage();
         double newDamage = Math.floor(oldDamage / 2);
-        if(newDamage < 1)
+        if (newDamage < 1)
             newDamage = 1;
         event.setDamage(newDamage);
         System.out.println(String.format("[DMG] OLD: [%.2f] NEW: [%.2f]", oldDamage, newDamage));
     }
 
     @EventHandler
-    public void onRespawn(PlayerRespawnEvent event){
+    public void onRespawn(PlayerRespawnEvent event) {
         UHC.arena.spectate(event.getPlayer());
-        Bukkit.getServer().getScheduler().runTaskLater(UHC.plugin, ()->{
-            if(!event.getPlayer().hasPotionEffect(PotionEffectType.INVISIBILITY)){
+        Bukkit.getServer().getScheduler().runTaskLater(UHC.plugin, () -> {
+            if (!event.getPlayer().hasPotionEffect(PotionEffectType.INVISIBILITY)) {
                 UHC.arena.spectate(event.getPlayer());
             }
         }, 10L);
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void entityInteract(PlayerInteractEvent evt) {
+        if (evt.getAction() == Action.RIGHT_CLICK_AIR || evt.getAction() == Action.RIGHT_CLICK_BLOCK)
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                if (!TeamHandler.isSpectator(p))
+                    continue;
+                Block clickedAgainst = evt.getClickedBlock();
+                BlockFace clickedFace = evt.getBlockFace();
+                Block newBlock = clickedAgainst.getWorld().getBlockAt(clickedAgainst.getX() + clickedFace.getModX(), clickedAgainst.getY() + clickedFace.getModY(), clickedAgainst.getZ() + clickedFace.getModZ());
+                // Check if there is a player at this block
+                double dist = newBlock.getLocation().distanceSquared(p.getLocation());
+                Location newLoc = p.getLocation().clone().add(0, 1, 0);
+                double newDist = newBlock.getLocation().distanceSquared(newLoc);
+                if (dist < 2 || newDist < 0.7225) {
+                    Location loc = p.getLocation().clone();
+                    double toAdd = 1.3;
+                    if (newDist < 0.7225)
+                        toAdd += 0.5;
+                    loc.setY(loc.getY() + toAdd);
+                    p.sendMessage(ChatColor.BLUE + "> " + ChatColor.GRAY + "You are in the way of a player and have been moved");
+                    p.teleport(loc);
+                }
+            }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void blockPlace(BlockPlaceEvent evt) {
+    }
+
     @EventHandler
-    public void changeWorld(PlayerChangedWorldEvent event){
-        if(!event.getFrom().getName().contains("nether")){
+    public void changeWorld(PlayerChangedWorldEvent event) {
+        if (!event.getFrom().getName().contains("nether")) {
             return;
         }
         double bounds = UHC.arena.getWorld().getWorldBorder().getSize() / 2;
-        System.out.println("Bounds: +/- "+bounds);
+        System.out.println("Bounds: +/- " + bounds);
         Player player = event.getPlayer();
-        if(Math.abs(player.getLocation().getBlockZ()) > bounds || Math.abs(player.getLocation().getBlockX()) > bounds){
+        if (Math.abs(player.getLocation().getBlockZ()) > bounds || Math.abs(player.getLocation().getBlockX()) > bounds) {
             System.out.println("Player has spawned outside the worldborder! Fixing");
             // Move the player diagonally into the worldborder
             Location toTeleport = player.getLocation().clone();
             System.out.println(String.format("Old Location: %s - %.2f, %.2f, %.2f", toTeleport.getWorld().getName(), toTeleport.getX(), toTeleport.getY(), toTeleport.getZ()));
             int attempts = 0;
-            while(Math.abs(toTeleport.getBlockX()) > bounds-2) {
-                if(toTeleport.getX() < 0){
+            while (Math.abs(toTeleport.getBlockX()) > bounds - 2) {
+                if (toTeleport.getX() < 0) {
                     toTeleport.setX(toTeleport.getX() + 0.5);
                 } else {
                     toTeleport.setX(toTeleport.getX() - 0.5);
                 }
                 attempts++;
-                if(attempts > 15000){
+                if (attempts > 15000) {
                     attempts = 0;
                     System.out.println("GIVING UP ON X");
                     break; // Give up
                 }
             }
-            while(Math.abs(toTeleport.getBlockZ()) > bounds-2){
-                if(toTeleport.getZ() < 0){
+            while (Math.abs(toTeleport.getBlockZ()) > bounds - 2) {
+                if (toTeleport.getZ() < 0) {
                     toTeleport.setZ(toTeleport.getZ() + 0.5);
                 } else {
                     toTeleport.setZ(toTeleport.getZ() - 0.5);
                 }
                 attempts++;
-                if(attempts > 15000){
+                if (attempts > 15000) {
                     System.out.println("GIVING UP ON Z");
                     break; // Give up
                 }
@@ -124,7 +157,7 @@ public class GameListener implements Listener {
             toTeleport = event.getPlayer().getWorld().getHighestBlockAt(toTeleport).getLocation().add(0.5, 0.5, 0.5);
             System.out.println(String.format("New Location: %s - %.2f, %.2f, %.2f", toTeleport.getWorld().getName(), toTeleport.getX(), toTeleport.getY(), toTeleport.getZ()));
             event.getPlayer().teleport(toTeleport);
-            event.getPlayer().sendMessage(ChatColor.GOLD+"You would've spawned outside of the world border, so we moved you back in");
+            event.getPlayer().sendMessage(ChatColor.GOLD + "You would've spawned outside of the world border, so we moved you back in");
         }
     }
 }
