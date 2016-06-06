25a5158e9efa8a38a204afec0cdaa8c1bb2dbe0d
mrkirby153 <mr.austinwhyte@gmail.com>
2016-06-05 19:35:33 -0700

    Only assign the scoreboard if the login was successful

--- src/main/java/me/mrkirby153/kcuhc/scoreboard/ScoreboardManager.java
+++ src/main/java/me/mrkirby153/kcuhc/scoreboard/ScoreboardManager.java
@@ -3,13 +3,15 @@ package me.mrkirby153.kcuhc.scoreboard;
 import me.mrkirby153.kcuhc.UHC;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public class ScoreboardManager implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void playerLogin(PlayerLoginEvent event) {
-        Bukkit.getServer().getScheduler().runTaskLater(UHC.plugin, () -> event.getPlayer().setScoreboard(UHC.arena.scoreboard.getBoard()), 10);
+        if (event.getResult() == PlayerLoginEvent.Result.ALLOWED)
+            Bukkit.getServer().getScheduler().runTaskLater(UHC.plugin, () -> event.getPlayer().setScoreboard(UHC.arena.scoreboard.getBoard()), 10);
     }
 }
