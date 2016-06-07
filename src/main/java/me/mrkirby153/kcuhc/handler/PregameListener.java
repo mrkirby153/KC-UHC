77eba07468e47946c3af1486720ae6e258ac743e
mrkirby153 <mr.austinwhyte@gmail.com>
2016-06-07 11:06:22 -0700

    Deny pregame access to more things and allow creative mode to bypass

--- src/main/java/me/mrkirby153/kcuhc/handler/PregameListener.java
+++ src/main/java/me/mrkirby153/kcuhc/handler/PregameListener.java
@@ -4,15 +4,22 @@ import me.mrkirby153.kcuhc.UHC;
 import me.mrkirby153.kcuhc.arena.UHCArena;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
+import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
 import java.io.File;
@@ -22,6 +29,8 @@ public class PregameListener implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+            return;
         event.setCancelled(true);
     }
 
@@ -51,8 +60,8 @@ public class PregameListener implements Listener {
     }
 
     @EventHandler
-    public void onLogout(PlayerQuitEvent event){
-        if(UHC.arena.currentState() != UHCArena.State.RUNNING){
+    public void onLogout(PlayerQuitEvent event) {
+        if (UHC.arena.currentState() != UHCArena.State.RUNNING) {
             // Remove the player from the arena
             UHC.arena.removePlayer(event.getPlayer());
         }
@@ -60,7 +69,7 @@ public class PregameListener implements Listener {
 
     @EventHandler
     public void entityDamage(EntityDamageEvent event) {
-        if(event.getCause() == EntityDamageEvent.DamageCause.VOID)
+        if (event.getCause() == EntityDamageEvent.DamageCause.VOID)
             return;
         if (event.getEntity() instanceof Player) {
             if (event.getEntity().getFireTicks() > 0)
@@ -76,16 +85,78 @@ public class PregameListener implements Listener {
 
     @EventHandler
     public void itemPickup(PlayerPickupItemEvent event) {
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+            return;
         event.setCancelled(true);
     }
 
     @EventHandler
     public void itemDrop(PlayerDropItemEvent event) {
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+            return;
         event.setCancelled(true);
     }
 
     @EventHandler
     public void blockPlace(BlockPlaceEvent event) {
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+            return;
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void entityTarget(EntityTargetEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void hangingBreak(HangingBreakByEntityEvent event) {
+        if (event.getRemover().getType() == EntityType.PLAYER) {
+            if (((Player) event.getRemover()).getGameMode() == GameMode.CREATIVE)
+                return;
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void vehicleDestroy(VehicleDestroyEvent event) {
+        if (event.getAttacker() instanceof Player) {
+            if (((Player) event.getAttacker()).getGameMode() == GameMode.CREATIVE)
+                return;
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void vehiclePush(VehicleEntityCollisionEvent event){
+        if(event.getEntity() instanceof Player){
+            if(((Player) event.getEntity()).getGameMode() == GameMode.CREATIVE)
+                return;
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void vehicleDamage(VehicleDamageEvent event) {
+        if (event.getAttacker() instanceof Player) {
+            if (((Player) event.getAttacker()).getGameMode() == GameMode.CREATIVE) {
+                return;
+            }
+        }
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void entityInteract(PlayerInteractEntityEvent event){
+        if(event.getPlayer().getGameMode() == GameMode.CREATIVE)
+            return;
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+            return;
         event.setCancelled(true);
     }
 
