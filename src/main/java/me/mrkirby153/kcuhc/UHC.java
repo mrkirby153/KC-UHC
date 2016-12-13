867a609356ee25528abd1e13792af29d0a81ce48
mrkirby153 <mr.austinwhyte@gmail.com>
2016-12-12 17:26:43 -0800

    Move Discord integration to a module

--- src/main/java/me/mrkirby153/kcuhc/UHC.java
+++ src/main/java/me/mrkirby153/kcuhc/UHC.java
@@ -3,19 +3,15 @@ package me.mrkirby153.kcuhc;
 import me.mrkirby153.kcuhc.arena.UHCArena;
 import me.mrkirby153.kcuhc.command.*;
 import me.mrkirby153.kcuhc.handler.*;
-import me.mrkirby153.kcuhc.handler.listener.SpectateListener;
 import me.mrkirby153.kcuhc.scoreboard.ScoreboardManager;
 import me.mrkirby153.kcuhc.team.TeamHandler;
 import me.mrkirby153.kcutils.BossBar;
 import me.mrkirby153.uhc.bot.network.UHCNetwork;
-import me.mrkirby153.uhc.bot.network.data.RedisConnection;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class UHC extends JavaPlugin {
 
@@ -23,7 +19,6 @@ public class UHC extends JavaPlugin {
     public static UHCNetwork uhcNetwork;
     private static UHC plugin;
     public UHCArena arena;
-    public SpectateListener spectateListener;
     public PlayerTrackerHandler playerTracker;
     public VelocityTracker velocityTracker;
     public ExtraHealthHandler extraHealthHelper;
@@ -34,6 +29,7 @@ public class UHC extends JavaPlugin {
     public EpisodeMarkerHandler markerHandler;
     public SpectatorHandler spectatorHandler;
     public BossBar bossBar;
+    public DiscordHandler discordHandler;
 
     public static UHC getInstance() {
         return plugin;
@@ -69,24 +65,10 @@ public class UHC extends JavaPlugin {
             arena.initialize();
         }
 
-        if (getConfig().getBoolean("discord.useDiscord")) {
-            getCommand("discord").setExecutor(new CommandDiscord(teamHandler, plugin));
-            String botHost = getConfig().getString("discord.botHost");
-            int botPort = getConfig().getInt("discord.botPort");
-            String password = getConfig().getString("discord.botPassword");
-            uhcNetwork = new UHCNetwork(new RedisConnection(botHost, botPort, password.equals("") ? null : password));
-            if (getConfig().getString("discord.serverId") == null || getConfig().getString("discord.serverId").isEmpty()) {
-                String acceptableChars = "ABCEDFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-                Random r = new SecureRandom();
-                String id = "";
-                for (int i = 0; i < 15; i++) {
-                    id += acceptableChars.charAt(r.nextInt(acceptableChars.length()));
-                }
-                getConfig().set("discord.serverId", id);
-                saveConfig();
-                getLogger().info("Set server id to " + id);
-            }
-        }
+        // Load discord integration
+        discordHandler = new DiscordHandler(plugin, teamHandler);
+        discordHandler.load();
+        uhcNetwork = discordHandler.network;
 
         new FreezeHandler(this);
 
