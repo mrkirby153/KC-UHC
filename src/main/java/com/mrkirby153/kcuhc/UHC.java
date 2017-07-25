22b72a60d1e981027ed5a2e1a39cf34d21c22ea9
mrkirby153 <mr.austinwhyte@gmail.com>
2017-07-25 13:08:47 -0700

    Initialize the world flag module

--- src/main/java/com/mrkirby153/kcuhc/UHC.java
+++ src/main/java/com/mrkirby153/kcuhc/UHC.java
@@ -1,8 +1,10 @@
 package com.mrkirby153.kcuhc;
 
 import co.aikar.commands.BukkitCommandManager;
+import com.mrkirby153.kcuhc.game.UHCGame;
 import me.mrkirby153.kcutils.command.CommandManager;
 import me.mrkirby153.kcutils.event.UpdateEventHandler;
+import me.mrkirby153.kcutils.flags.FlagModule;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UHC extends JavaPlugin {
@@ -10,6 +12,18 @@ public class UHC extends JavaPlugin {
     private static BukkitCommandManager manager;
 
     private UpdateEventHandler tickEventHandler;
+    private FlagModule flagModule;
+
+    private UHCGame game;
+
+    /**
+     * Gets the {@link UHCGame}
+     *
+     * @return The game
+     */
+    public UHCGame getGame() {
+        return game;
+    }
 
     @Override
     public void onEnable() {
@@ -24,5 +38,12 @@ public class UHC extends JavaPlugin {
         tickEventHandler = new UpdateEventHandler(this);
         tickEventHandler.load();
 
+        // Initialize the world flags
+        flagModule = new FlagModule(this);
+        flagModule.load();
+        flagModule.initialize(getServer().getWorlds().get(0));
+
+        // Initialize the game
+        game = new UHCGame();
     }
 }
