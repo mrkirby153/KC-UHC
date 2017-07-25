df2ce3b6ac152695f372b5a66b957a52563628ba
mrkirby153 <mr.austinwhyte@gmail.com>
2017-07-25 12:30:06 -0700

    Initialize tick event

--- src/main/java/com/mrkirby153/kcuhc/UHC.java
+++ src/main/java/com/mrkirby153/kcuhc/UHC.java
@@ -2,11 +2,14 @@ package com.mrkirby153.kcuhc;
 
 import co.aikar.commands.BukkitCommandManager;
 import me.mrkirby153.kcutils.command.CommandManager;
+import me.mrkirby153.kcutils.event.UpdateEventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UHC extends JavaPlugin {
 
     private static BukkitCommandManager manager;
+
+    private UpdateEventHandler tickEventHandler;
 
     @Override
     public void onEnable() {
@@ -16,5 +19,10 @@ public class UHC extends JavaPlugin {
         CommandManager.initialize(this);
         // Initialize ACF
         manager = new BukkitCommandManager(this);
+
+        // Initialize tick event
+        tickEventHandler = new UpdateEventHandler(this);
+        tickEventHandler.load();
+
     }
 }
