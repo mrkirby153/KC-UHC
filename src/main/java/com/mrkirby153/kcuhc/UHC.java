025fbcc707195cf5769f91455358923aba6b6766
mrkirby153 <mr.austinwhyte@gmail.com>
2017-07-25 12:23:43 -0700

    Initialize configuration and command handler

--- src/main/java/com/mrkirby153/kcuhc/UHC.java
+++ src/main/java/com/mrkirby153/kcuhc/UHC.java
@@ -1,6 +1,20 @@
 package com.mrkirby153.kcuhc;
 
+import co.aikar.commands.BukkitCommandManager;
+import me.mrkirby153.kcutils.command.CommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UHC extends JavaPlugin {
+
+    private static BukkitCommandManager manager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        // Initialize the command manager
+        CommandManager.initialize(this);
+        // Initialize ACF
+        manager = new BukkitCommandManager(this);
+    }
 }
