92d6254be5265421619d5e6b7b07228bf8bf15a3
mrkirby153 <mr.austinwhyte@gmail.com>
2017-07-26 21:19:52 -0700

    Add worldborder module

--- src/main/java/com/mrkirby153/kcuhc/game/GameCommand.java
+++ src/main/java/com/mrkirby153/kcuhc/game/GameCommand.java
@@ -5,7 +5,10 @@ import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.Subcommand;
 import com.mrkirby153.kcuhc.UHC;
+import com.mrkirby153.kcuhc.module.ModuleRegistry;
+import com.mrkirby153.kcuhc.module.worldborder.WorldBorderModule;
 import me.mrkirby153.kcutils.C;
+import me.mrkirby153.kcutils.Time;
 import org.bukkit.command.CommandSender;
 
 @CommandAlias("game")
@@ -25,5 +28,55 @@ public class GameCommand extends BaseCommand {
     public void setGameState(CommandSender sender, GameState state){
         game.setCurrentState(state);
         sender.sendMessage(C.m("Game", "Set game state to {state}", "{state}", state).toLegacyText());
+    }
+
+    @Subcommand("border")
+    public class BorderCommands extends BaseCommand {
+
+        @Subcommand("get")
+        public void getBorder(CommandSender sender){
+            if(!ModuleRegistry.INSTANCE.loaded(WorldBorderModule.class)){
+                sender.sendMessage(C.e("Worldborder is not loaded!").toLegacyText());
+                return;
+            }
+            WorldBorderModule module = ModuleRegistry.INSTANCE.getModule(WorldBorderModule.class);
+            sender.sendMessage(C.m("Worldborder", "Worldborder will move from {start} to {end} in {time}",
+                    "{start}", module.getStartSize(),
+                    "{end}", module.getEndSize(),
+                    "{time}", Time.format(2, module.getDuration() * 1000, Time.TimeUnit.FIT)).toLegacyText());
+        }
+
+        @Subcommand("set")
+        public void setBorder(CommandSender sender, Integer start, Integer end, Integer time){
+            if(game.getCurrentState() != GameState.WAITING){
+                sender.sendMessage(C.e("You cannot change the worldborder while the game is in motion!").toLegacyText());
+            }
+            if(!ModuleRegistry.INSTANCE.loaded(WorldBorderModule.class)){
+                sender.sendMessage(C.e("Worldborder is not loaded!").toLegacyText());
+                return;
+            }
+            WorldBorderModule module = ModuleRegistry.INSTANCE.getModule(WorldBorderModule.class);
+            module.setStartSize(start);
+            module.setEndSize(end);
+            module.setDuration(time);
+            sender.sendMessage(C.m("Worldborder", "Worldborder will move from {start} to {end} in {time}",
+                    "{start}", module.getStartSize(),
+                    "{end}", module.getEndSize(),
+                    "{time}", Time.format(2, module.getDuration() * 1000, Time.TimeUnit.FIT)).toLegacyText());
+        }
+
+        @Subcommand("update")
+        public void newTime(CommandSender sender, Integer newTime){
+            if(!ModuleRegistry.INSTANCE.loaded(WorldBorderModule.class)){
+                sender.sendMessage(C.e("Worldborder is not loaded!").toLegacyText());
+                return;
+            }
+            if(game.getCurrentState() != GameState.ALIVE){
+                sender.sendMessage(C.e("This command can only be used when the game is alive!").toLegacyText());
+                return;
+            }
+            WorldBorderModule module = ModuleRegistry.INSTANCE.getModule(WorldBorderModule.class);
+            module.updateSpeed(newTime);
+        }
     }
 }
---
+++ src/main/java/com/mrkirby153/kcuhc/module/worldborder/WorldBorderModule.java
@@ -0,0 +1,118 @@
+package com.mrkirby153.kcuhc.module.worldborder;
+
+import com.mrkirby153.kcuhc.UHC;
+import com.mrkirby153.kcuhc.game.GameState;
+import com.mrkirby153.kcuhc.game.event.GameStateChangeEvent;
+import com.mrkirby153.kcuhc.module.UHCModule;
+import me.mrkirby153.kcutils.C;
+import me.mrkirby153.kcutils.Time;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.WorldBorder;
+import org.bukkit.event.EventHandler;
+
+public class WorldBorderModule extends UHCModule {
+
+    private static final int LOBBY_SIZE = 60;
+    private static final int DEFAULT_SIZE = 60000;
+
+    private int startSize = 100;
+    private int endSize = 50;
+
+    private int duration = 300; // Default to 5 minutes
+
+    public WorldBorderModule() {
+        super("World Border", Material.BARRIER);
+    }
+
+    /**
+     * Gets the time it will take for the border to move from start to end
+     *
+     * @return The time, in seconds
+     */
+    public int getDuration() {
+        return duration;
+    }
+
+    /**
+     * Sets the duration of the border
+     *
+     * @param duration The duration
+     */
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * Gets the end size of the border
+     *
+     * @return The end size of the border
+     */
+    public int getEndSize() {
+        return endSize;
+    }
+
+    /**
+     * Sets the end size of the border
+     *
+     * @param endSize The end size of the border
+     */
+    public void setEndSize(int endSize) {
+        this.endSize = endSize;
+    }
+
+    /**
+     * Gets the start size of the worldborder
+     *
+     * @return The start size of the border
+     */
+    public int getStartSize() {
+        return startSize;
+    }
+
+    /**
+     * Sets the start size of the border
+     *
+     * @param startSize The size of the border
+     */
+    public void setStartSize(int startSize) {
+        this.startSize = startSize;
+    }
+
+    @EventHandler
+    public void onGameStateChange(GameStateChangeEvent event) {
+        if (event.getTo() == GameState.ENDING || event.getTo() == GameState.WAITING) {
+            UHC.getUHCWorld().getWorldBorder().setSize(LOBBY_SIZE);
+        }
+        if (event.getTo() == GameState.ALIVE) {
+            UHC.getUHCWorld().getWorldBorder().setSize(startSize);
+            UHC.getUHCWorld().getWorldBorder().setSize(endSize, duration);
+            System.out.println("Moving " + (startSize - endSize) + " blocks in " + duration + " seconds");
+        }
+    }
+
+    @Override
+    public void onLoad() {
+        UHC.getUHCWorld().getWorldBorder().setSize(LOBBY_SIZE);
+    }
+
+    @Override
+    public void onUnload() {
+        UHC.getUHCWorld().getWorldBorder().setSize(DEFAULT_SIZE);
+    }
+
+    public void updateSpeed(int newDuration) {
+        WorldBorder border = UHC.getUHCWorld().getWorldBorder();
+        double size = border.getSize();
+        border.setSize(size); // Reset the border to its current position
+        Bukkit.getOnlinePlayers().forEach(p -> {
+            p.spigot().sendMessage(C.m("World Border", "Alert! Moving from {startSize} to {endSize} in {duration}",
+                    "{startSize}", size,
+                    "{endSize}", endSize,
+                    "{duration}", Time.format(1, newDuration * 1000, Time.TimeUnit.FIT)));
+            p.playSound(p.getLocation(), Sound.BLOCK_NOTE_PLING, 1F, 1F);
+        });
+        border.setSize(endSize, newDuration);
+    }
+}
