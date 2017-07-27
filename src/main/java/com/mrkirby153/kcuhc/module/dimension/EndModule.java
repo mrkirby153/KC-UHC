f2d3c1e2789dd05d649d00350a5dc090d82298e4
mrkirby153 <mr.austinwhyte@gmail.com>
2017-07-26 21:47:49 -0700

    Add dimension disable modules

---
+++ src/main/java/com/mrkirby153/kcuhc/module/dimension/EndModule.java
@@ -0,0 +1,30 @@
+package com.mrkirby153.kcuhc.module.dimension;
+
+import com.mrkirby153.kcuhc.module.UHCModule;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityPortalEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
+
+public class EndModule extends UHCModule {
+
+    public EndModule(){
+        super("Disable End", "Disables the end", Material.EYE_OF_ENDER);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPortal(EntityPortalEvent event){
+        if(event.getTo().getWorld().getEnvironment() == World.Environment.THE_END){
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerPortal(PlayerPortalEvent event){
+        if(event.getTo().getWorld().getEnvironment() == World.Environment.THE_END){
+            event.setCancelled(true);
+        }
+    }
+}
---
+++ src/main/java/com/mrkirby153/kcuhc/module/dimension/NetherModule.java
@@ -0,0 +1,30 @@
+package com.mrkirby153.kcuhc.module.dimension;
+
+import com.mrkirby153.kcuhc.module.UHCModule;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityPortalEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
+
+public class NetherModule extends UHCModule {
+
+    public NetherModule(){
+        super("Disable Nether", "Disables the nether", Material.NETHER_BRICK);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPortal(EntityPortalEvent event){
+        if(event.getTo().getWorld().getEnvironment() == World.Environment.NETHER){
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerPortal(PlayerPortalEvent event){
+        if(event.getTo().getWorld().getEnvironment() == World.Environment.NETHER){
+            event.setCancelled(true);
+        }
+    }
+}
