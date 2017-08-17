ae1724cbfcfb4e3a3c558803bb7ac2c50cf40a1e
mrkirby153 <mr.austinwhyte@gmail.com>
2017-08-16 18:22:07 -0700

    Change scoreboard formatting

--- src/main/java/com/mrkirby153/kcuhc/scoreboard/ScoreboardUpdater.java
+++ src/main/java/com/mrkirby153/kcuhc/scoreboard/ScoreboardUpdater.java
@@ -4,37 +4,39 @@ import com.google.inject.Inject;
 import com.mrkirby153.kcuhc.UHC;
 import com.mrkirby153.kcuhc.game.GameState;
 import com.mrkirby153.kcuhc.game.UHCGame;
-import com.mrkirby153.kcuhc.game.team.SpectatorTeam;
 import com.mrkirby153.kcuhc.game.team.UHCTeam;
 import com.mrkirby153.kcuhc.module.ModuleRegistry;
+import com.mrkirby153.kcuhc.module.player.PvPGraceModule;
 import com.mrkirby153.kcuhc.module.worldborder.WorldBorderModule;
 import me.mrkirby153.kcutils.Time;
 import me.mrkirby153.kcutils.event.UpdateEvent;
 import me.mrkirby153.kcutils.event.UpdateType;
-import me.mrkirby153.kcutils.scoreboard.KirbyScoreboard;
-import me.mrkirby153.kcutils.scoreboard.ScoreboardTeam;
 import me.mrkirby153.kcutils.scoreboard.items.ElementHeadedText;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * The scoreboard updater
  */
 public class ScoreboardUpdater implements Listener {
 
-    private final KirbyScoreboard scoreboard;
+    private final UHCScoreboard scoreboard;
     private final UHCGame game;
+    private final UHC uhc;
+
 
     @Inject
     public ScoreboardUpdater(UHC plugin, UHCGame game) {
         this.scoreboard = new UHCScoreboard(plugin);
         this.game = game;
+        this.uhc = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
@@ -61,6 +63,12 @@ public class ScoreboardUpdater implements Listener {
                 scoreboard.add(" ");
                 break;
             case ALIVE:
+                ModuleRegistry.INSTANCE.getLoadedModule(PvPGraceModule.class).ifPresent(mod -> {
+                    if (mod.getGraceTimeRemaining() > 0) {
+                        scoreboard.add(" ");
+                        scoreboard.add(new ElementHeadedText(ChatColor.AQUA + "" + ChatColor.BOLD + "PvP Enabled in", Time.format(1, mod.getGraceTimeRemaining(), Time.TimeUnit.FIT)));
+                    }
+                });
                 scoreboard.add(" ");
                 // 9 slots for teams/players/etc.
                 List<UHCTeam> aliveTeams = new ArrayList<>();
@@ -68,30 +76,7 @@ public class ScoreboardUpdater implements Listener {
                     if (t.getPlayers().size() > 0)
                         aliveTeams.add(t);
                 });
-                List<Player> alivePlayers = new ArrayList<>();
-                // Players not on the spectators team
-                Bukkit.getServer().getOnlinePlayers().stream().filter(Player::isValid).filter(p -> {
-                    ScoreboardTeam team = game.getTeam(p);
-                    return team == null || !(team instanceof SpectatorTeam);
-                }).forEach(alivePlayers::add);
-                if (alivePlayers.size() < 9) {
-                    // Display players sorted by health
-                    alivePlayers.sort((o1, o2) -> (int) (o2.getHealth() - o1.getHealth()));
-                    alivePlayers.forEach(p -> {
-                        UHCTeam team = (UHCTeam) game.getTeam(p);
-                        ChatColor color = team != null ? team.getColor() : ChatColor.WHITE;
-                        scoreboard.add(color + p.getName() + " " + ChatColor.RED + (int) p.getHealth() + " " + Character.toString('\u2764'));
-                    });
-
-                } else if (aliveTeams.size() < 9) {
-                    // Display alive teams sorted by players
-                    aliveTeams.sort((o1, o2) -> o2.getPlayers().size() - o1.getPlayers().size());
-                    aliveTeams.forEach(t -> scoreboard.add(t.getColor() + t.getTeamName() + ChatColor.RED + " - " + t.getPlayers().size() + ((t.getPlayers().size() == 1) ? " player" : " players")));
-                } else {
-                    // Display alive team count
-                    scoreboard.add(new ElementHeadedText(ChatColor.AQUA + "" + ChatColor.BOLD + "Teams",
-                            String.format(ChatColor.GOLD + "%s" + ChatColor.WHITE + " alive", aliveTeams.size())));
-                }
+                scoreboard.add(ChatColor.GREEN + "Teams Alive: " + ChatColor.WHITE + aliveTeams.size());
                 scoreboard.add(" ");
                 ModuleRegistry.INSTANCE.getLoadedModule(WorldBorderModule.class).ifPresent(worldBorderModule -> {
                     scoreboard.add(new ElementHeadedText(ChatColor.YELLOW + "" + ChatColor.BOLD + "World Border",
